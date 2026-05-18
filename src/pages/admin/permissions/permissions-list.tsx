56fad5674a3baa15a8dfe3b88a6e919bc970c1ee
Reuben Wafula <rubewafula@gmail.com>
2026-05-18 22:08:29 +0300

    Add framework for permission checking

--- src/pages/admin/permissions/permissions-list.tsx
+++ src/pages/admin/permissions/permissions-list.tsx
@@ -1,11 +1,21 @@
-import { List, DataTable, EditButton, DeleteButton, TextInput, required, Toolbar } from 'react-admin';
+import { List, DataTable, EditButton, DeleteButton, TextInput, required, Toolbar, useResourceContext } from 'react-admin';
 import { CreateButton } from '../../../components/forms/FormUtils';
 
 import Tooltip from '@mui/material/Tooltip';
 import { Stack } from '@mui/material';
+import { useCan } from '../../../components/permissions/user-can';
 
 
-export const PermissionList = () => (
+export const PermissionList = () => {
+
+    const can = useCan();
+
+    const resource = useResourceContext() ?? "none";
+    const canEdit = can(resource, "update");
+    const canDelete = can(resource, "delete");
+
+    return (
+
     <List 
         title="Permissions"
         actions={
@@ -21,24 +31,30 @@ export const PermissionList = () => (
             <DataTable.Col label="Actions">
                  <Stack direction="row" spacing={1} alignItems="center">
 
-                    <Tooltip title ="Edit Record">
-                        <span>
-                            <EditButton label={false}  />
-                        </span>
-                    </Tooltip>
-                    <Tooltip title ="Delete Record">
-                        <span>
-                            <DeleteButton  
-                            label={false} 
-                            mutationMode="pessimistic"
-                            confirmTitle="⚠️ Confirm deletion"
-                            confirmContent="This will permanently remove the record."
-                            />
-                        </span>
-                    </Tooltip>
+                   {canEdit && (
+                        <Tooltip title="Edit Record">
+                            <span>
+                                <EditButton label={false} />
+                            </span>
+                        </Tooltip>
+                    )}
+
+                    {canDelete && (
+                        <Tooltip title="Delete Record">
+                            <span>
+                                <DeleteButton
+                                    label={false}
+                                    mutationMode="pessimistic"
+                                    confirmTitle="⚠️ Confirm deletion"
+                                    confirmContent="This will permanently remove the record."
+                                />
+                            </span>
+                        </Tooltip>
+                    )}
                 </Stack>
                 
             </DataTable.Col>
         </DataTable>
     </List>
 );
+}

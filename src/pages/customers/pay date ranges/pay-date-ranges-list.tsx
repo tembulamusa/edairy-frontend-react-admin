77aa495661bf8c47a5968230540130dd80e5f739
nexusnovatech <nexusnovacreek@gmail.com>
2026-05-27 20:59:35 +0300

    Table fixes

--- src/pages/customers/pay date ranges/pay-date-ranges-list.tsx
+++ src/pages/customers/pay date ranges/pay-date-ranges-list.tsx
@@ -1,4 +1,4 @@
-import { DataTable, DateField, NumberField } from 'react-admin';
+import { DataTable, DateField, TextField } from 'react-admin';
 import { CustomersListWrapper } from '../shared/customers-list-layout';
 
 export const CustomerPayDateRangeList = () => (
@@ -6,16 +6,17 @@ export const CustomerPayDateRangeList = () => (
         title="Customer Pay Date Ranges"
         subtitle="Define billing periods used for customer billings and collections"
     >
-        <DataTable.Col source="name" label="Period Name" />
+        <DataTable.Col source="name" label="Period Name">
+            <TextField source="name" />
+        </DataTable.Col>
         <DataTable.Col source="start_date" label="Start Date">
             <DateField source="start_date" />
         </DataTable.Col>
         <DataTable.Col source="end_date" label="End Date">
             <DateField source="end_date" />
         </DataTable.Col>
-        <DataTable.Col source="rate" label="Rate">
-            <NumberField source="rate" />
+        <DataTable.Col source="created_at" label="Created At">
+            <DateField source="created_at" />
         </DataTable.Col>
-        <DataTable.Col source="description" label="Description" />
     </CustomersListWrapper>
 );
